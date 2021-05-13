de44b80c9da1722b5ff79f2499203228e99209d2
Jack Wang <2232123545@qq.com/jack3shihong@gmail.com>
2021-05-13 07:51:55 -0700

    a

--- chapter2/first.c
+++ chapter2/first.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
+int print_words(int num);
+
 int main(void){
 
     int num;
     num = 1;
 
+    printf("%d", num);
+    print_words(num);
+
+    return 0;
+}
+
+int print_words(int num){
     printf("I am a simple");
     printf("computer.\n");
-    printf("My favorite number is %d because it is first", num);
-
+    printf("My favorite number is %d because it is first\n", num);
     return 0;
 }
