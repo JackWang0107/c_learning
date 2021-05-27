972f36708be7e7fdee7d16eb63bd7643fb051792
Jack Wang <2232123545@qq.com/jack3shihong@gmail.com>
2021-05-27 00:26:23 -0700

    finished part of chapter8 -- By Jack Wang 2021.5.27 15:26

---
+++ chapter9/recur.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void up_and_down(int);
+
+int main(void){
+    up_and_down(1);
+
+    return 0;
+}
+
+
+void up_and_down(int n){
+    printf("Leve; %d: n location %p\n", n, &n);
+    if (n < 4)
+        up_and_down(n+1);
+    printf("Leve; %d: n location %p\n", n, &n);
+}
